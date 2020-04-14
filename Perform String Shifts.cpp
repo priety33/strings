14889dbdb6fc6af7ec12b2800b8a664fd6e83b76
priety33 <52635177+priety33@users.noreply.github.com>
2020-04-14 15:47:31 +0530

    Update Perform String Shifts.cpp

--- Perform String Shifts.cpp
+++ Perform String Shifts.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    
-    void leftshift(string &s, int a)
-    {
-        if(a==0) return;
-        s+=s[0];
-        s.erase(0,1);
-        leftshift(s, a-1);
-    }
-    
-    void rightshift(string &s, int a)
-    {
-        if(a==0) return;
-        s=s[s.size()-1]+s;
-        s.erase(s.size()-1, 1);
-        rightshift(s, a-1);
-    }
-    
-    string stringShift(string s, vector<vector<int>>& shift) {
-        if(shift.size()==0) return s;
-        int n=shift.size();
-        for(int i=0; i<n; i++)
-        {
-            if(shift[i][0]==0) leftshift(s, shift[i][1]);
-            else rightshift(s, shift[i][1]);
+    string stringShift(string str, vector<vector<int>>& shift) {
+        int s=0;
+        for(int i=0;i<shift.size();i++){
+            if(shift[i][0]==0)s-=shift[i][1]; //left shift
+            else s+=shift[i][1]; //right shift
         }
-        return s;
+        cout << s <<endl;
+        if(s<0){
+            s=-1*s;
+            s=s%str.size();
+            str=str.substr(s,str.size()-s)+str.substr(0,s);
+            return str;
+        }
+        s=s%str.size();
+        if(s==0) return str;
+        str=str.substr(str.size()-s,s)+str.substr(0,str.size()-s);
+        return str;
     }
 };
